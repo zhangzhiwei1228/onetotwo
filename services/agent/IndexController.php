857e114ccaa4d6fd3fb0e064dccf0fe52cc3f7ec
= <=>
2016-06-13 16:54:24 +0800

    统计消费

--- services/agent/IndexController.php
+++ services/agent/IndexController.php
@@ -49,7 +49,17 @@ class Agent_IndexController extends Agent_Controller_Action
 		$view->render('views/merchants/merchants_staffw.php');
 	}
 	public function doShopList() {
+		if($this->user['role'] != 'resale') $this->redirect(isset($this->_request->ref) ? base64_decode($this->_request->ref) : 'action=default');
+		$start_time = strtotime(date('Y-m-01 00:00:00', time()));
+		$end_time = strtotime(date('Y-m-d 23:59:59',strtotime(date('Y-m-01 23:59:59', time()).' +1 month -1 day')));
+		$merchants = M('User')->alias('u')
+			->leftJoin(M('User_Credit')->getTableName().' AS ct', 'ct.user_id = u.id')
+			->group('ct.user_id')
+			->columns('u.*, SUM(ct.credit) AS remain, SUM(IF(ct.credit > 0, ct.credit, 0)) AS recharge, SUM(IF(ct.credit < 0, ct.credit, 0)) AS consume, ct.id as ct_id')
+			->where('u.parent_id = '.$this->user['id'].' and ct.create_time <= '.$end_time.' and ct.create_time >= '.$start_time)
+			->order('u.id DESC');
 		$view = $this->_initView();
+		$view->merchants = $merchants->fetchRows();;
 		$view->render('views/shoplist.php');
 	}
 }
--- wwwroot/themes/123mf/views/shoplist.php
+++ wwwroot/themes/123mf/views/shoplist.php
@@ -1,211 +1,54 @@
 <!DOCTYPE html>
 <html>
 <head>
-<?php include_once VIEWS.'inc/head.php'; ?>
+	<?php include_once VIEWS.'inc/head.php'; ?>
 </head>
 <body>
-	<div class="n-personal-center-tit">
-		<a href="javascript:history.go(-1);"><img width="11" height="21" src="<?php echo static_file('mobile/img/img-22.png'); ?> " alt=""></a>
-		员工列表
-	</div>
-	<table class="n-shoplist-table1" width="100%">
-		<tr>
-			<th>商家名称</th>
-			<th>
-				<p>本月充值</p>
-				<p>免费积分</p>
-			</th>
-			<th>
-				<p>本月使用</p>
-				<p>免费积分</p>
-			</th>
-			<th>
-				<p>免费积分</p>
-				<p>余额</p>
-			</th>
+<div class="n-personal-center-tit">
+	<a href="javascript:history.go(-1);"><img width="11" height="21" src="<?php echo static_file('mobile/img/img-22.png'); ?> " alt=""></a>
+	员工列表
+</div>
+<table class="n-shoplist-table1" width="100%">
+
+	<tr>
+		<th>商家名称</th>
+		<th>
+			<p>本月充值</p>
+			<p>免费积分</p>
+		</th>
+		<th>
+			<p>本月使用</p>
+			<p>免费积分</p>
+		</th>
+		<th>
+			<p>免费积分</p>
+			<p>余额</p>
+		</th>
+	</tr>
+	<?php if (!count($this->merchants)) { ?>
+		<tr align="center">
+			<td colspan="4"><div class="notfound">找不到相关信息</div></td>
 		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
+	<?php } else {foreach ($this->merchants as $row) {?>
 		<tr>
 			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
+				<p style="text-align:left;margin-left:10%;"><?php echo $row['nickname'];?></p>
 			</td>
 			<td>
-				100000
+				<?php echo $row['recharge']?>
 			</td>
 			<td>
-				50000
+				<?php echo $row['consume']?>
 			</td>
 			<td>
-				50000
+				<?php echo $row['remain'];?>
 			</td>
 		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<p style="text-align:left;margin-left:10%;">爱尚自助烤肉</p>
-				<p style="text-align:left;margin-left:10%;">火锅</p>
-			</td>
-			<td>
-				100000
-			</td>
-			<td>
-				50000
-			</td>
-			<td>
-				50000
-			</td>
-		</tr>
-	</table>
-	<div class="n-h56"></div>
-	<?php //include_once VIEWS.'inc/footer_merchants.php'; ?>
+	<?php } }?>
+
+
+</table>
+<div class="n-h56"></div>
+<?php //include_once VIEWS.'inc/footer_merchants.php'; ?>
 </body>
 </html>
